29ff51379f3e006716e461b21d996294af18f290
tombryntesen@gmail.com <tombryntesen@gmail.com@f95074ba-ac62-05f4-7a77-6c0d08563508>
2012-02-13 13:21:06 +0000

    -use snap mutator for transform -created enum mutator

--- trunk/src/trb/fps/property/MutateLabel.java
+++ trunk/src/trb/fps/property/MutateLabel.java
@@ -4,6 +4,7 @@ import java.awt.Cursor;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 import javax.swing.JLabel;
 
 public class MutateLabel {
@@ -13,13 +14,13 @@ public class MutateLabel {
     private final Mutator mutator;
     private static final Mutator DEFAULT_MUTATOR = new NumberMutator();
     private Point dragStart;
-    private Number valueStart;
+    private Object valueStart;
 
     private MouseAdapter listener = new MouseAdapter() {
         @Override
         public void mousePressed(MouseEvent e) {
             dragStart = e.getPoint();
-            valueStart = (Number) property.get();
+            valueStart = property.get();
         }
 
         @Override
@@ -31,6 +32,11 @@ public class MutateLabel {
         public void mouseDragged(MouseEvent e) {
             property.set(mutator.mutate(property.getType(), valueStart, e.getPoint().x - dragStart.x));
         }
+
+		@Override
+		public void mouseWheelMoved(MouseWheelEvent e) {
+			property.set(mutator.mutate(property.getType(), property.get(), -e.getWheelRotation()));
+		}
     };
 
     public MutateLabel(Property p) {
@@ -39,11 +45,21 @@ public class MutateLabel {
 
     public MutateLabel(Property p, Mutator mutator) {
         this.property = p;
-        this.mutator = (mutator != null) ? mutator : DEFAULT_MUTATOR;
+		Mutator newMutator = mutator;
+		if (newMutator == null) {
+			if (Enum.class.isAssignableFrom(p.getType())) {
+				newMutator = new EnumMutator();
+				System.out.println("XXXXXXXXXXXXXX");
+			} else {
+				newMutator = DEFAULT_MUTATOR;
+			}
+		}
+        this.mutator = newMutator;
         label = new JLabel(p.getName());
         label.setCursor(new Cursor(Cursor.E_RESIZE_CURSOR));
         label.addMouseListener(listener);
         label.addMouseMotionListener(listener);
+		label.addMouseWheelListener(listener);
     }
 
     public JLabel get() {
